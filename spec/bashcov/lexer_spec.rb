89e34654235e35040e3ca7fa4980a50fd93f5aa6
Cédric Félizard <cedric@felizard.fr>
2012-12-13 17:59:33 +0100

    Neater specs using shared examples

--- spec/bashcov/lexer_spec.rb
+++ spec/bashcov/lexer_spec.rb
@@ -1,20 +1,28 @@
 require 'spec_helper'
 
-describe Bashcov::Lexer do
+shared_examples "a bash file" do
   describe "#irrelevant_lines" do
-    [
-      ['simple.sh', [0, 1, 2, 3, 6, 8, 9, 11, 12]],
-      ['function.sh', [0, 1, 2, 4, 5, 6, 9, 10, 13]],
-      ['sourced.txt', [0, 1, 3]]
-
-    ].each do |filename, lines|
-      context "for #{filename}" do
-        it "returns irrelevant lines" do
-          lexer = Bashcov::Lexer.new File.join(scripts, filename)
-          lexer.irrelevant_lines.should =~ lines
-        end
-      end
+    it "returns irrelevant lines" do
+      lexer = Bashcov::Lexer.new File.join(scripts, filename)
+      lexer.irrelevant_lines.should =~ irrelevant_lines
     end
+  end
+end
+
+describe Bashcov::Lexer do
+  it_behaves_like "a bash file" do
+    let(:filename) { 'simple.sh' }
+    let(:irrelevant_lines) { [0, 1, 2, 3, 6, 8, 9, 11, 12] }
+  end
+
+  it_behaves_like "a bash file" do
+    let(:filename) { 'function.sh' }
+    let(:irrelevant_lines) { [0, 1, 2, 4, 5, 6, 9, 10, 13] }
+  end
+
+  it_behaves_like "a bash file" do
+    let(:filename) { 'sourced.txt' }
+    let(:irrelevant_lines) { [0, 1, 3] }
   end
 end
 
