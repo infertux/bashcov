6850f65de093bf63eb79ea09397dfd1f531c9991
Cédric Félizard <cedric@felizard.fr>
2013-03-11 00:39:40 +0100

    Long methods are unbearable

--- lib/bashcov.rb
+++ lib/bashcov.rb
@@ -46,25 +46,28 @@ module Bashcov
 
   private
 
+    def help program_name
+      <<-HELP.gsub!(/^ +/, '').gsub!("\t", ' ' * 4)
+        Usage: #{program_name} [options] [--] <command> [options]
+        Examples:
+        \t#{program_name} ./script.sh
+        \t#{program_name} --skip-uncovered ./script.sh
+        \t#{program_name} -- ./script.sh --some --flags
+        \t#{program_name} --skip-uncovered -- ./script.sh --some --flags
+      HELP
+    end
+
     def option_parser
       OptionParser.new do |opts|
         opts.program_name = 'bashcov'
         opts.version = Bashcov::VERSION
-        opts.banner = <<-HELP.gsub!(/^ +/, '').gsub!("\t", ' ' * 4)
-          Usage: #{opts.program_name} [options] [--] <command> [options]
-          Examples:
-          \t#{opts.program_name} ./script.sh
-          \t#{opts.program_name} --skip-uncovered ./script.sh
-          \t#{opts.program_name} -- ./script.sh --some --flags
-          \t#{opts.program_name} --skip-uncovered -- ./script.sh --some --flags
-        HELP
+        opts.banner = help opts.program_name
 
         opts.separator "\nSpecific options:"
 
         opts.on("-s", "--skip-uncovered", "Do not report uncovered files") do |s|
           @options.skip_uncovered = s
         end
-
         opts.on("-m", "--mute", "Do not print script output") do |m|
           @options.mute = m
         end
@@ -74,7 +77,6 @@ module Bashcov
         opts.on_tail("-h", "--help", "Show this message") do
           abort(opts.help)
         end
-
         opts.on_tail("--version", "Show version") do
           puts opts.ver
           exit
