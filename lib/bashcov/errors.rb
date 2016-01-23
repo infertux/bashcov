d5f13b3f8de5e81f43f7db74659de7ecbcc5343f
Matt Schreiber <EMAIL>
2016-01-23 09:40:00 -0500

    Added error class for indicating failure to parse Xtrace output

---
+++ lib/bashcov/errors.rb
@@ -0,0 +1,14 @@
+module Bashcov
+  # Signals an error parsing Bash's debugging output.
+  class XtraceError < ::RuntimeError
+    # Will contain the coverages parsed prior to the error
+    attr_reader :files
+
+    # @param [#to_s]  message An error message
+    # @param [Hash]   files   A hash containing coverage information
+    def initialize(message, files)
+      @files = files
+      super(message)
+    end
+  end
+end
