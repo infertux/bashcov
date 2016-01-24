47911cc7f0a60988449494fc6c93b9f61d12cad9
Matt Schreiber <EMAIL>
2016-01-23 19:17:33 -0500

    inject_xtrace_flag! now executes a provided block/proc and restores SHELLOPTS after completion. Also, #run is now sensitive to the availability of BASH_XTRACEFD

--- lib/bashcov/runner.rb
+++ lib/bashcov/runner.rb
@@ -10,39 +10,63 @@ module Bashcov
     # @note Binds Bashcov +stdin+ to the program being executed.
     # @return [Process::Status] Status of the executed command
     def run
-      inject_xtrace_flag!
-
       @xtrace = Xtrace.new
       fd = @xtrace.file_descriptor
-      options = { :in => :in, fd => fd } # bind FDs to the child process
-      if Bashcov.options.mute
-        options[:out] = "/dev/null"
-        options[:err] = "/dev/null"
+      env = { "PS4" => Xtrace::PS4, "SHELL" => Bashcov.options.bash_path }
+      options = { in: :in }
+
+      # Older versions of Bash don't have the +BASH_XTRACEFD+ environment
+      # variable
+      if Bashcov.bash_xtracefd?
+        options[fd] = fd # bind FDs to the child process
+
+        if Bashcov.options.mute
+          options[:out] = "/dev/null"
+          options[:err] = "/dev/null"
+        end
+
+        env["BASH_XTRACEFD"] = fd.to_s
+      else
+        options[:err] = fd
+
+        if Bashcov.options.mute
+          options[:out] = "/dev/null"
+        else # don't bother issuing warning if we're silencing output anyway
+          $stderr.puts <<-ERROR.gsub(/^\s+/, "").lines.map { |s| s.chomp("\n") }.join(" ")
+            Warning: you are using an older Bash version that does not support
+            BASH_XTRACEFD. All xtrace output will print to standard error, and
+            your script's output on standard error will not be printed to the
+            console.
+          ERROR
+        end
       end
-      env = { "BASH_XTRACEFD" => fd.to_s, "PS4" => Xtrace::PS4 }
 
-      command_pid = Process.spawn env, @command, options # spawn the command
+      # Now inject the xtrace flag -- any earlier and we would have seen it
+      # when querying the Bash version info
+      inject_xtrace_flag! do
+        command_pid = Process.spawn env, @command, options # spawn the command
 
-      begin
-        xtrace_thread = Thread.new { @xtrace.read } # start processing the xtrace output
+        begin
+          xtrace_thread = Thread.new { @xtrace.read } # start processing the xtrace output
 
-        Process.wait command_pid
+          Process.wait command_pid
 
-        @xtrace.close
+          @xtrace.close
 
-        @coverage = xtrace_thread.value # wait for the thread to return
-      rescue XtraceError => e
-        $stderr.puts <<-ERROR.gsub!(/^\s+/, "").lines.map { |s| s.chomp("\n") }.join(" ")
-          Warning: encountered an error parsing Bash's output(error was:
-          #{e.message}). This can occur if your script or its path contains the
-          sequence #{Xtrace::DELIM}, or if your script unsets LINENO. Aborting
-          early; coverage report will be incomplete.
-        ERROR
+          @coverage = xtrace_thread.value # wait for the thread to return
+        rescue XtraceError => e
+          $stderr.puts <<-ERROR.gsub(/^\s+/, "").lines.map { |s| s.chomp("\n") }.join(" ")
+            Warning: encountered an error parsing Bash's output(error was:
+            #{e.message}). This can occur if your script or its path contains the
+            sequence #{Xtrace::DELIM}, or if your script unsets LINENO. Aborting
+            early; coverage report will be incomplete.
+          ERROR
 
-        @coverage = e.files
+          @coverage = e.files
+        end
+
+        $?
       end
-
-      $?
     end
 
     # @return [Hash] Coverage hash of the last run
@@ -60,10 +84,20 @@ module Bashcov
   private
 
     # @note +SHELLOPTS+ must be exported so we use Ruby's {ENV} variable
-    # @return [void]
+    # @yield [void] adds "xtrace" to +SHELLOPTS+ and then runs the provided
+    #   block
+    # @return [Object, ...] the value returned by the calling block
     def inject_xtrace_flag!
-      existing_flags = (ENV["SHELLOPTS"] || "").split(":")
+      return enum_for(__method__) unless block_given?
+
+      existing_flags_s = ENV["SHELLOPTS"]
+      existing_flags = (existing_flags_s || "").split(":")
       ENV["SHELLOPTS"] = (existing_flags | ["xtrace"]).join(":")
+
+      # Calls the provided block
+      Proc.new.call
+    ensure
+      ENV["SHELLOPTS"] = existing_flags_s
     end
 
     # Add files which have not been executed at all (i.e. with no coverage)
