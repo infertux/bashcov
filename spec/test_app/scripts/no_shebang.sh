54fa46626d090d6f6d1ece6f13ab0a11b3a52c70
Cédric Félizard <cedric@felizard.fr>
2023-04-10 15:12:59 +0000

    Don't support scripts without a shebang
    They seem to have a different behaviour when running as root which makes
    debugging a pain. All proper Bash scripts should have a shebang anyway.

--- spec/test_app/scripts/no_shebang.sh
+++
@@ -1,5 +0,0 @@
-# Tests that Bashcov::Detective properly detects shell scripts with
-#   1. A shellscript extension like .sh or .bash *and*
-#   2. Contents that follow valid shell syntax
-
-echo "no shebang here!"
